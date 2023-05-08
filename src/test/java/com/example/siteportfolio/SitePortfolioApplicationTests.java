975640289523bfc537fec040906809a7e63d746e
Eduarda Souto <101151329+EduardaSouto28@users.noreply.github.com>
2023-05-07 22:50:52 -0300

    feat: init my portfolio with spring

---
+++ src/test/java/com/example/siteportfolio/SitePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siteportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
